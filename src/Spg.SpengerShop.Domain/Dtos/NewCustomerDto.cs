b7aba2067af5009e86ca9852e7111b5bc54ad991
schrutek <martin@schrutek>
2023-01-27 10:01:17 +0100

    Fluent Validation NuGetPackage added

---
+++ src/Spg.SpengerShop.Domain/Dtos/NewCustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using Spg.SpengerShop.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.SpengerShop.Domain.Dtos
+{
+    public class NewCustomerDto
+    {
+        public GenderTypesDto Gender { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EMail { get; set; } = string.Empty;
+        public PhoneNumber? PhoneNumber { get; set; }
+        public string SocialSecurityNumber { get; set; } = string.Empty;
+    }
+}
